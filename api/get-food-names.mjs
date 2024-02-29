e265a0dffb0b4dbba3fc18b56d463a92fd54ce02
Jolomi <test@example.com>
2024-02-29 13:17:20 +0000

    correct get food names

--- api/get-food-names.mjs
+++ api/get-food-names.mjs
@@ -1,11 +1,14 @@
 import { fetchFoodNames } from './foodNamesDB.mjs';
 
-exports.handler = async (event) => {
+async function handler(event) {
   try {
     const foodNames = await fetchFoodNames();
     return { statusCode: 200, body: JSON.stringify(foodNames) };
   } catch (error) {
     return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
   }
-};
+}
+
+export { handler };
+
 
